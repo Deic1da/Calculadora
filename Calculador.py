8ac6b9d5a4b39c4797054dcf2402cd490badfa8d
Deic1da <melquener@gmail.com>
2024-09-07 16:10:15 -0300

    É até facil...

--- Calculador.py
+++ Calculador.py
@@ -16,26 +16,23 @@ if (operacao == 1):
         pergunta = input("Deseja adicionar mais um valor?(S/N) \n").lower()
     print(f"A soma dos valores digitados é = {entrada}")
 
-if (operacao == 2):
+elif (operacao == 2):
     entrada = float(input("Digite o valor : "))
     while(pergunta != "n"):
         entrada -= float(input("Digite o valor : "))
         pergunta = input("Deseja adicionar mais um valor?(S/N) \n").lower()
     print(f"A Subtração dos valores digitados é = {entrada}")
 
-if (operacao == 3):
+elif (operacao == 3):
     entrada = float(input("Digite o valor : "))
     while(pergunta != "n"):
         entrada /= float(input("Digite o valor : "))
         pergunta = input("Deseja adicionar mais um valor?(S/N) \n").lower()
     print(f"A Divisão dos valores digitados é = {entrada}")
     
-if (operacao == 4):
+elif (operacao == 4):
     entrada = float(input("Digite o valor : "))
     while(pergunta != "n"):
         entrada *= float(input("Digite o valor: "))
         pergunta = input("Deseja adicionar mais um valor?(S/N) \n").lower()
     print(f"A Multiplicação dos valores digitados é = {entrada}")
-    
-    #então quer dizer que não posso ficar salvando o que não tem alteração e eu quase enlouqueci com isso?
-    
