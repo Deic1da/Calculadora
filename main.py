cf258978e64de2f5b7163be14d850e689a4b6aaa
Deic1da <melquener@gmail.com>
2024-09-14 15:50:27 -0300

    Final

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from utils.calculadora import *
+
+total = None
+
 print("1 - Somar\n2 - subtrair\n3 - multiplicar\n4 - dividir\n")
 operacao = int(input("Qual o peração deseja usar: "))
+
 x = float(input("Digite o primeiro valor: "))
 y = float(input("Digite o segundo valor: "))
+
+if operacao == 1:
+    total = somar(x, y)
+elif operacao == 2:
+    total = subtrair(x, y)
+elif operacao == 3:
+    total = multiplicar(x, y)
+elif operacao == 4:
+    total = dividir(x, y)
+
+print(total)
---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+from .calculadora import somar, subtrair, multiplicar, dividir
+
+__all__ = ['somar', 'subtrair', 'multiplicar', 'dividir']
