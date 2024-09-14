1ce37e5ff22cbf2b353aa1609369a087369dcd1c
Deic1da <melquener@gmail.com>
2024-09-14 15:32:13 -0300

    Espero que não tenha visto ainda

---
+++ main.py
@@ -0,0 +1,5 @@
+from utils.calculadora import *
+print("1 - Somar\n2 - subtrair\n3 - multiplicar\n4 - dividir\n")
+operacao = int(input("Qual o peração deseja usar: "))
+x = float(input("Digite o primeiro valor: "))
+y = float(input("Digite o segundo valor: "))
